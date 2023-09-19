b9abc61ea1c2894cce89e02ec4a068327b6d5ccb
RThomas4565 <thom1148@purdue.edu>
2023-09-19 14:47:44 -0400

    Lab5.1

--- ThomasR_Lab5/ThomasR_Lab5.cpp
+++ ThomasR_Lab5/ThomasR_Lab5.cpp
@@ -1,20 +1,72 @@
-// ThomasR_Lab5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+
+
+
+
+
 
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+char translateChar(char inputChar, const std::unordered_map<char, char>& cipherTranslation) {
+    char uppercaseInputChar = inputChar;
+    if (inputChar >= 'a' && inputChar <= 'z') {
+        uppercaseInputChar = inputChar - 'a' + 'A';
+    }
+
+    if (cipherTranslation.find(uppercaseInputChar) != cipherTranslation.end()) {
+        char translatedChar = cipherTranslation.at(uppercaseInputChar);
+        if (inputChar >= 'a' && inputChar <= 'z') {
+            translatedChar = translatedChar - 'A' + 'a';
+        }
+        return translatedChar;
+    }
+
+    return inputChar; 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    std::unordered_map<char, char> cipherTranslation = {
+        {'A', 'V'},
+        {'B', 'F'},
+        {'C', 'X'},
+        {'D', 'B'},
+        {'E', 'L'},
+        {'F', 'I'},
+        {'G', 'T'},
+        {'H', 'Z'},
+        {'I', 'J'},
+        {'J', 'R'},
+        {'K', 'P'},
+        {'L', 'H'},
+        {'M', 'D'},
+        {'N', 'K'},
+        {'O', 'N'},
+        {'P', 'O'},
+        {'Q', 'W'},
+        {'R', 'S'},
+        {'S', 'G'},
+        {'T', 'U'},
+        {'U', 'Y'},
+        {'V', 'Q'},
+        {'W', 'M'},
+        {'X', 'A'},
+        {'Y', 'C'},
+        {'Z', 'E'}
+    };
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    std::string userInput;
+
+    std::cout << "Enter the text to translate: ";
+    std::getline(std::cin, userInput);
+
+    std::cout << "Translated Text: ";
+    for (char c : userInput) {
+        char translatedChar = translateChar(c, cipherTranslation);
+        std::cout << translatedChar;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
